e270afcf1eab05961450763d1f16aa7aa832b10e
zentarg <apg.chili@gmail.com>
2020-09-15 23:31:32 +0200

    Overrides Car Price() method to give 20% discount on weekends.

---
+++ StoreBaeltTicketLibrary/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class Car : ClassLibrary.Car
+    {
+        /// <inheritdoc />
+        public Car(string licensePlate, DateTime date, bool brobizzPresent) : base(licensePlate, date, brobizzPresent)
+        {
+        }
+
+        public override double Price()
+        {
+            if (Date.DayOfWeek == DayOfWeek.Saturday || Date.DayOfWeek == DayOfWeek.Sunday)
+                return base.Price() * 0.8;
+            return base.Price();
+        }
+    }
+}
